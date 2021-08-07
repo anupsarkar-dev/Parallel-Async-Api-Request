fc8720f866287f9e500b51a86e2f5c2d62e54831
Anup Sarkar <aronno1107@gmail.com>
2021-08-07 21:49:32 +0600

    Initial commit

---
+++ ConsoleToTestAPi/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleToTestAPi
+{
+    class Program
+    {
+        static async  Task Main(string[] args)
+        {
+
+
+            Console.WriteLine("Wait For 5 Seconds");
+
+            Task.Delay(50000);
+
+
+            Console.WriteLine("Now Sending Request");
+
+            HttpClient httpClient = new HttpClient();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            var Task1 = await GetTask1(httpClient);
+            var Task2 = await GetTask2(httpClient);
+            var Task3 = await GetTask3(httpClient);
+
+
+            Console.WriteLine($"Total Time Taken To Process : {stopwatch.ElapsedMilliseconds}ms");
+            Console.WriteLine($"{Task1} , {Task2} , {Task3} ");
+        }
+
+        public static async Task<int> GetTask1(HttpClient httpClient)
+        {
+            var response = await httpClient.GetStringAsync("http://localhost:5000/weatherforecast/get1");
+            var data = JsonSerializer.Deserialize<ResponseData>(response);
+
+            return data!.Data;
+        }
+
+
+        public static async Task<int> GetTask2(HttpClient httpClient)
+        {
+            var response = await httpClient.GetStringAsync("http://localhost:5000/weatherforecast/get2");
+            var data = JsonSerializer.Deserialize<ResponseData>(response);
+
+            return data!.Data;
+        }
+
+
+        public static async Task<int> GetTask3(HttpClient httpClient)
+        {
+            var response = await httpClient.GetStringAsync("http://localhost:5000/weatherforecast/get3");
+            var data = JsonSerializer.Deserialize<ResponseData>(response);
+
+            return data!.Data;
+        }
+    }
+
+    class ResponseData
+    {
+        public int Data { get; set; }
+    }
+}
---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("Get1")]
+        public  async Task<IActionResult> Get1()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            return Ok(new {Data=20000});
+        }
+
+        [HttpGet("Get2")]
+        public async Task<IActionResult> Get2()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            return Ok(new { Data = 20000 });
+        }
+
+
+        [HttpGet("Get3")]
+        public async Task<IActionResult> Get3()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            return Ok(new { Data = 30000 });
+        }
+    }
+}
